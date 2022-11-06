bf5ad5e8b9c29fa17968da74ea55f084eefb2121
Medvesajt <116150806+tankoczipeter@users.noreply.github.com>
2022-11-06 16:39:37 +0100

    Update hazi.py

--- hazi.py
+++ hazi.py
@@ -1,4 +1,4 @@
-#1. Feladat: Készíts egy programot! A gép gondol egy számra 1 és 5 között, vagyis egy változóban tárolj egy ilyen számot!
+#1.
 
 szam = int(input("Kérem adjon meg egy számot 1-10-ig!:"))
 
@@ -10,4 +10,42 @@ if szam >= 10:
     
 if szam <= 1:
     print("Nagyobb")
+   
+#2.    
+x = 5
+y = -3
+  
+  if x < 0 and y < 0:
+    print('mindkettő negatív')
+  
+  if x < 0 or y < 0:
+    print('van köztük negatív')
+  
+  if not x <= 0:
+    print('x pozitív')     
+      
+ #3.
+  import random
+
+  random_szam = random.randint(1,10)
+  print(random_szam)      
+    
+ #4.
+ szam = 1
+  while szam <= 10:
+    print(szam)
+    szam = szam + 1    
+    
+ #5
+x = 10
+y = -1
+  
+  if x < 0 and y < 0:
+    print('mindkettő negatív')
+  
+  if x < 0 or y < 0:
+    print('van köztük negatív')
+  
+  
+  
     
